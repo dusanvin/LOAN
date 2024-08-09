db3333fb21a8a2efcb8129d4e7688c0ff301d267
lumlop <vincent.dusanek@gmail.com>
2024-08-09 11:20:26 +0200

    Update edit.blade.php
    Add "Sonstiges" to Edit Device

--- resources/views/devices/edit.blade.php
+++ resources/views/devices/edit.blade.php
@@ -52,6 +52,7 @@
                     <option value="Koffer" {{ $device->group == 'Koffer' ? 'selected' : '' }}>Koffer</option>
                     <option value="Laptop" {{ $device->group == 'Laptop' ? 'selected' : '' }}>Laptop</option>
                     <option value="Tablet" {{ $device->group == 'Tablet' ? 'selected' : '' }}>Tablet</option>
+                    <option value="Sonstiges" {{ $device->group == 'Sonstiges' ? 'selected' : '' }}>Sonstiges</option>
                 </select>
             </div>
 
